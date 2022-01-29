676c2b81e55e708e7938acdf8c4d7db68db1c1cb
Hiromi Ishii <konn.jinro@gmail.com>
2022-01-29 20:56:04 +0900

    Reorganises CI and adds GHC 9.2.1 (#5)
    * Reorganises CI and adds GHC 9.2.1
    
    * scheduled
    
    * HSpec
    
    * Replaces hspec with tasty
    
    * tasty-inspection-testing
    
    * Bumps up

--- test/Shared.hs
+++ test/Shared.hs
@@ -1,21 +1,28 @@
-{-# LANGUAGE DataKinds, TemplateHaskell #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -O2 -fno-hpc #-}
-{-# OPTIONS_GHC -dsuppress-idinfo -dsuppress-coercions
-      -dsuppress-type-applications
-      -dsuppress-module-prefixes -dsuppress-type-signatures
-      -dsuppress-uniques #-}
+
 module Shared where
-import Language.Haskell.TH
-import Test.Hspec
-import Test.Inspection
 
+import Language.Haskell.TH (ExpQ)
+import Test.Tasty.Inspection
 
-checkInspection
-  :: Result -> Expectation
-checkInspection Success{} = pure ()
-checkInspection (Failure msg) =
-  fail msg
+inspecting :: String -> Obligation -> ExpQ
+inspecting title obl = inspectTest $ obl {testName = Just title}
 
-inspecting :: String -> Obligation -> Q Exp
-inspecting desc reg =
-  [|it desc $ checkInspection $(inspectTest reg)|]
+data GHCVer = GHC8_8 | GHC8_10 | GHC9_0 | GHC9_2
+  deriving (Show, Eq, Ord)
+
+ghcVer :: GHCVer
+
+#if __GLASGOW_HASKELL__ == 902
+ghcVer = GHC9_2
+#elif __GLASGOW_HASKELL__ == 900
+ghcVer = GHC9_0
+#elif __GLASGOW_HASKELL__ == 810
+ghcVer = GHC8_10
+#elif __GLASGOW_HASKELL__ == 808
+ghcVer = GHC8_8
+#else
+ghcVer = error "Coudld not determine GHC Version: __GLASGOW_HASKELL__"
+#endif
