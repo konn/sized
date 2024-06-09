2dfb7c492b1429115b4ba57e94b54b61b939b915
Hiromi ISHII <konn.jinro@gmail.com>
2024-06-09 21:32:53 +0900

    Supports GHC 9.10

--- test/Shared.hs
+++ test/Shared.hs
@@ -10,11 +10,13 @@ import Test.Tasty.Inspection
 inspecting :: String -> Obligation -> ExpQ
 inspecting title obl = inspectTest $ obl {testName = Just title}
 
-data GHCVer = GHC8_8 | GHC8_10 | GHC9_0 | GHC9_2 | GHC9_4 | GHC9_6 | GHC9_8
+data GHCVer = GHC8_8 | GHC8_10 | GHC9_0 | GHC9_2 | GHC9_4 | GHC9_6 | GHC9_8 | GHC9_10
   deriving (Show, Eq, Ord)
 
 ghcVer :: GHCVer
-#if __GLASGOW_HASKELL__ == 908
+#if __GLASGOW_HASKELL__ == 910
+ghcVer = GHC9_10
+#elif __GLASGOW_HASKELL__ == 908
 ghcVer = GHC9_8
 #elif __GLASGOW_HASKELL__ == 906
 ghcVer = GHC9_6
